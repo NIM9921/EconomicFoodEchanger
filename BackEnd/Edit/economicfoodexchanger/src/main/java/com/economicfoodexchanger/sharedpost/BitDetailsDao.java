672864a0adcdba33e02277ac83265f99d49528df
Akila Madhurangana weerasinghe <60591310+Akila-gif@users.noreply.github.com>
2025-06-13 01:48:53 +0530

    Update Bid Dealer Requests
    Update Bid Dealer Requests. table after adding

--- BackEnd/Edit/economicfoodexchanger/src/main/java/com/economicfoodexchanger/sharedpost/BitDetailsDao.java
+++ BackEnd/Edit/economicfoodexchanger/src/main/java/com/economicfoodexchanger/sharedpost/BitDetailsDao.java
@@ -1,7 +1,10 @@
 package com.economicfoodexchanger.sharedpost;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface BitDetailsDao extends JpaRepository<BitDetails, Integer> {
 
 
+    List<BitDetails> findBySharedpost(SharedPost sharedPost);
 }
