eea9d9c43a0a4861a545941d8e718069820c159c
Akila Madhurangana weerasinghe <60591310+Akila-gif@users.noreply.github.com>
2025-09-15 04:06:48 +0530

    Client side AWS update

--- FrontEnd/my-admin-app/src/utils/ApiConfig.ts
+++ FrontEnd/my-admin-app/src/utils/ApiConfig.ts
@@ -4,7 +4,8 @@
  */
 class ApiConfig {
     // Base API Domain
-    public static readonly Domain = 'http://localhost:8080';
+    //public static readonly Domain = 'http://localhost:8080';
+    public static readonly Domain = 'http://44.223.66.178:8080';
 }
 
 export default ApiConfig;
