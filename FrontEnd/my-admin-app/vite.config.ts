387754892750d194c774ba94509e83dedcf452fb
Akila Madhurangana weerasinghe <60591310+Akila-gif@users.noreply.github.com>
2025-09-15 22:17:15 +0530

    Update SSL certificate

--- FrontEnd/my-admin-app/vite.config.ts
+++ FrontEnd/my-admin-app/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,6 +8,10 @@ export default defineConfig({
   server: {
     host: '0.0.0.0', // Listen on all network interfaces
     port: 5173,
+    https: {
+      key: fs.readFileSync('/home/ec2-user/ssl/dev.key'),
+      cert: fs.readFileSync('/home/ec2-user/ssl/dev.crt'),
+    },
     allowedHosts: [
       'ae9d-101-2-191-170.ngrok-free.app' // Add your ngrok domain here
     ]
